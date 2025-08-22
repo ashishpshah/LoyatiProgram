24389b6e8581560dc9b308f5de4fc136fa2da522
Kaval Patel <Kaval@padhyasoft.com>
2025-08-22 10:59:01 +0530

    Changes_22082025_1058

--- Models/LoyaltyPointScheme.cs
+++ Models/LoyaltyPointScheme.cs
@@ -6,12 +6,16 @@ namespace Seed_Admin
     {
         public override long Id { get; set; }
         public  long ProductID { get; set; }
+        public long PackageType_ID { get; set; }
+        public long SKUSize_ID { get; set; }
         public int MaxPurchaseQty { get; set; }
         public int MinPurchaseQty { get; set; }
         public int LoyaltyPoints { get; set; }
         public DateTime? EffectiveStartDate { get; set; }
         public DateTime? EffectiveEndDate { get; set; }
         [NotMapped]public string Product_Name { get; set; }
+        [NotMapped] public string PackageType_Name { get; set; }
+        [NotMapped] public string SKUSize_Name { get; set; }
 
     }
 }
